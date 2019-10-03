40f168e3a161e0ca6b3ae8a9c48b8f1ca5b686b3
Dhana <dhana_dhira@apps.ipb.ac.id>
2019-10-03 17:59:29 +0700

    Add Product 2 numbers cpp

---
+++ Turbo C++/Product_of_two_numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream.h>
+
+void main()
+{
+  int a, b, c;
+  cout << "Enter two integers to get the product\n";
+  cin >> a >> b;
+  c = a * b;
+
+  cout <<"Product of the numbers: " << c << endl;
+  return 0;
+}
